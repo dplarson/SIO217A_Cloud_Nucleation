dc4902ec2633300d9f0a6c7067af982518837e2e
David Larson <dplarson@ucsd.edu>
2013-11-14 15:22:08 -0800

    Improve variable naming consistency

--- code/calculateDropSize.m
+++ code/calculateDropSize.m
@@ -1,83 +1,78 @@
-function [resultValues, timeValues] = calculateDropSize(initialMass, saturationRatio, temperature, finalTime)
+function [rDroplet, tDroplet] = calculateDropSize(m_solt, S, T, tEnd)
 %CalculateDropSize Calculates the drop size increase
 %   Uses the Equation 5.26 and solves iteratively.
 %
 % Inputs:
-%   initialMass: mass of solute base [kg]
-%   saturationRatio: super saturaton [-]
-%   temperature: ambient temperature [K]
-%   finalTime: end time [s]
+%   m_solt: mass of solute (NaCl) [kg]
+%   S: super saturaton [-]
+%   T: ambient temperature [K]
+%   tEnd: end time [s]
 %
 % Outputs:
-%   resultValues: droplet radius vector [um]
-%   timeValues: time vector [s]
+%   rDroplet: droplet radius vector [um]
+%   tDroplet: time vector [s]
 %
 
 % Constants
-Rv = 461;             % gas constant [J/kg]
+R_v = 461;             % gas constant [J/kg]
 e_sInf = 609;         % saturation pressure [mb]
-T = temperature;      % ambient temperature [K]
-Rho_l = 1000;         % density water [kg/m^3]
-Rho_solt = 2160;      % density NaCl [kg/m^3]
-S = saturationRatio;  % super saturation [-]
-Sigma = 0.0761;       % surface tension [N/m]
+rho_l = 1000;         % density water [kg/m^3]
+rho_solt = 2160;      % density NaCl [kg/m^3]
+sigma = 0.0761;       % surface tension [N/m]
 M_v = 18;             % molecular weight water [g/mol]
 M_solt = 58.44;       % molecular weight NaCl [g/mol]
-m_solt = initialMass; % mass NaCl [kg]
 iConstant = 2;        % Van't Hoff Factor for NaCl [-]
 
 % Paramters at T=273K
-Kappa = 2.4*10^-2;    % coefficeint of thermal conductivity [J/(m*s*K)]
-Dv = 2.21*10^-5;      % coefficient of vapor diffusivity [m^2/s]
-L = 2.6*10^6;         % latent heat water [J/kg]
+kappa = 2.4*10^-2;    % coefficeint of thermal conductivity [J/(m*s*K)]
+D_v = 2.21*10^-5;      % coefficient of vapor diffusivity [m^2/s]
+L_lv = 2.6*10^6;         % latent heat water [J/kg]
 
 % radius of NaCl [m]
-r_solt = (m_solt * 3 / (4 * pi * Rho_solt))^(1/3);
+r_solt = (m_solt / (4 / 3 * pi * rho_solt))^(1/3);
 
 % time
-deltaT = 0.01;                          % time step [s]
-iterationNumber = finalTime / deltaT;   % number of iterations [-]
+tDelta = 0.01;               % time step [s]
+nIterations = tEnd / tDelta; % number of iterations [-]
 
 % initialize
-t0=0;
+t0 = 0;
 rValue = r_solt;
-rValueVector = zeros(iterationNumber,1)';
+rValueVector = zeros(nIterations, 1)';
 
 % solve Equation 5.26
-for i=1:iterationNumber
+for i=1:nIterations
 
     % Kelvin effect
-    a = 2 * Sigma/(Rho_l * Rv * T);
+    a = 2 * sigma / (rho_l * R_v * T);
 
     % mass of water
-    m_w = 4 / 3 * pi * Rho_l * (rValue^3);
+    m_w = 4 / 3 * pi * rho_l * (rValue^3);
 
     % number of moles water (n_w) and solute (n_s)
-    n_w = m_w/M_v;
-    n_s = iConstant*m_solt/M_solt;
+    n_w = m_w / M_v;
+    n_s = iConstant * m_solt / M_solt;
 
     % saturation pressure with solute
-    myConstant = n_w/(n_w+n_s);
-    e_s = e_sInf * exp(a/rValue) * myConstant;
+    e_s = e_sInf * exp(a / rValue) * n_w / (n_w + n_s);
 
     % heat conduction (K) and diffusion (D) [s/m^2]
-    K = L^2 * Rho_l / (T^2 * Kappa * Rv);
-    D = Rho_l * Rv * T / (Dv * e_s);
+    K = L_lv^2 * rho_l / (T^2 * kappa * R_v);
+    D = rho_l * R_v * T / (D_v * e_s);
 
-    ratio = (S - 1) / (K + D);
-    rValue = (ratio / rValue) * deltaT + rValue;
+    rValue = ((S - 1) / (K + D)) / rValue * tDelta + rValue;
 
     % convert from [m] to [um]
     rValueVector(i) = 1E6 * rValue;
 
     if rValue > 0.75*1E-6 && t0==0
-        t0 = i * deltaT;
+        t0 = i * tDelta;
     end
 end
 
 % organize results
-timeValues = deltaT*(1:iterationNumber);
-timeValues = timeValues(timeValues > t0);
-resultValues = rValueVector(timeValues > t0);
+tDroplet = tDelta * (1:nIterations);
+tDroplet = tDroplet(tDroplet > t0);
+rDroplet = rValueVector(tDroplet > t0);
 
 end
