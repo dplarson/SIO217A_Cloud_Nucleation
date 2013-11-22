704559c28da7940fbe83bc6efd2d66176f433f2f
David Larson <dplarson@ucsd.edu>
2013-11-22 01:05:26 -0800

    Add code for plotting T sensitivity
    Plotting r(t) as you varying temperature (T)

--- code/droplet.py
+++ code/droplet.py
@@ -44,7 +44,7 @@ rcParams.update(**params)
 # paper, includes both effects in their equation).
 #
 
-def droplet_radius(t, S_1=0.05, T=273.0, e_s=609.0):
+def droplet_radius(t, S_1=0.05, T=273.0, e_s=609.0, kappa=2.40E-2, D_v=2.21E-5, L_lv=2.5E6):
     """
     Calculate droplet radius over time.
 
@@ -57,12 +57,18 @@ def droplet_radius(t, S_1=0.05, T=273.0, e_s=609.0):
     T : float
         Ambient temperature [K]
     e_s : float
-        Saturation pressure [mb].
+        Saturation pressure [mb]
+    kappa : float
+        Coefficient of thermal conductivity [J/(m*s*K)]
+    D_v : float
+        Coefficient of diffusivity at p=1000 mb [m^2/s]
+    L_lv : float
+        Latent heat of water [J/kg]
 
     Returns
     -------
     r : ndarray
-        Droplet radius.
+        Droplet radius [um]
 
     """
 
@@ -71,58 +77,101 @@ def droplet_radius(t, S_1=0.05, T=273.0, e_s=609.0):
     p = 900.0  # ambient pressure [hPa]
     rho_l = 1000.0  # density of water [kg/m^3]
 
-    # Properties of water at T=273K
-    kappa = 2.40E-2  # coefficient of thermal conductivity [J/(m*s*K)]
-    D_v = 2.21E-5  # coefficient of vapor diffusivity [m^2/s]
-    L_lv = 2.60E6  # latent heat [J/kg]
-    sigma = 0.0761  # surface tension [N/m]
-
     # Parameters
-    m_solt = 1.0E-15  # mass of solute (NaCl) [kg]
-    M_solt = 58.44E-3  # molecular weight of solute (NaCl) [kg/mol]
-    M_l = 18.0  # molecular weight of water [kg/mol]
     r0 = 0.75E-6  # initial radius of the droplet [m]
     S_1 = S_1 / 100.0  # supersaturation [-]
+    D_v = D_v * (1.0E3 / p)  # adjust D_v
 
     K = (L_lv ** 2) * rho_l / (kappa * R * (T ** 2))
     D = rho_l * R * T / (e_s * D_v)
 
     r = np.sqrt((r0 ** 2) + (2 * S_1 / (K + D) * (t - t[0])))
 
-    return r
+    # return radius in microns
+    return (r * 1.0E6)
 
 
-t_start = 0.0
-t_end = 45.0E3
-t_delta = 1.0
-t = np.arange(t_start, t_end, t_delta)
-r = droplet_radius(t)
+def plot_table(t):
+    """
+    Plot model against Table 5.5 values.
 
-# Table 5.5 values
-r_true = [1, 2, 4, 10, 20, 30, 50]
-t_true14 = [2.4, 130, 1E3, 2.7E3, 8.5E3, 17.5E3, 44.5E3]
-t_true13 = [0.15, 7.0, 320, 1.8E3, 7.4E3, 16.0E3, 43.5E3]
-t_true12 = [0.013, 0.61, 62, 870, 5.9E3, 14.5E3, 41.5E3]
+    Parameters
+    ----------
+    t : ndarray
+        Time array.
 
-plt.figure()
-ax = plt.subplot(111)
+    """
+    r = droplet_radius(t, T=273.0, e_s=0.6E3)
 
-ax.plot(t, r * 1.0E6, ls='-', c='0.0')
-ax.plot(t_true14, r_true, ls='', marker='o', markerfacecolor='0.3')
-ax.plot(t_true13, r_true, ls='', marker='s', markerfacecolor='w', markeredgecolor='0.0')
-ax.plot(t_true12, r_true, ls='', marker='^', markerfacecolor='0.3')
+    # Table 5.5 values
+    r_true = [1, 2, 4, 10, 20, 30, 50]
+    t_true14 = [2.4, 130, 1E3, 2.7E3, 8.5E3, 17.5E3, 44.5E3]
+    t_true13 = [0.15, 7.0, 320, 1.8E3, 7.4E3, 16.0E3, 43.5E3]
+    t_true12 = [0.013, 0.61, 62, 870, 5.9E3, 14.5E3, 41.5E3]
 
-ax.set_xlabel('t [s]')
-ax.set_ylabel(r'r [$\mu m$]')
+    plt.figure()
+    ax = plt.subplot(111)
 
-ax.set_xticks([0.5E4, 1.5E4, 2.5E4, 3.5E4, 4.5E4])
+    ax.plot(t, r, ls='-', c='0.0')
+    ax.plot(t_true14, r_true, ls='', marker='o', markerfacecolor='0.3')
+    ax.plot(t_true13, r_true, ls='', marker='s', markerfacecolor='w', markeredgecolor='0.0')
+    ax.plot(t_true12, r_true, ls='', marker='^', markerfacecolor='0.3')
 
-# add legend above the plot and without a border
-#ax.legend(loc='upper center', ncol=4, bbox_to_anchor=(0.5, 1.0), frameon=False)
+    ax.set_xlabel('t [s]')
+    ax.set_ylabel(r'r [$\mu m$]')
 
-ax.grid()
+    ax.set_xticks([0.5E4, 1.5E4, 2.5E4, 3.5E4, 4.5E4])
 
-ax.set_ylim([0.0, 55.0])
+    # add legend above the plot and without a border
+    #ax.legend(loc='upper center', ncol=4, bbox_to_anchor=(0.5, 1.0), frameon=False)
 
-plt.savefig('r_t.pdf')
-#plt.show()
+    ax.grid()
+
+    ax.set_ylim([0.0, 55.0])
+
+    plt.savefig('r_t.pdf')
+    #plt.show()
+
+
+def plot_temperature(t):
+    """Plot r(t) for multiple temperatures."""
+
+    # r(t) with varying T
+    # NOTE: keep L_lv constant (for now)
+    r_273 = droplet_radius(t, T=273.0, e_s=6.15E2, kappa=2.40E-2, D_v=2.21E-6)
+    r_283 = droplet_radius(t, T=283.0, e_s=12.3E2, kappa=2.48E-2, D_v=2.36E-5)
+    r_293 = droplet_radius(t, T=293.0, e_s=23.4E2, kappa=2.55E-2, D_v=2.52E-5)
+    r_303 = droplet_radius(t, T=303.0, e_s=42.4E2, kappa=2.63E-2, D_v=2.69E-5)
+
+    plt.figure()
+    ax = plt.subplot(111)
+
+    ax.plot(t, r_273, ls='-', c='0.0')
+    ax.plot(t, r_283, ls=':',lw=1.5, c='0.0')
+    ax.plot(t, r_293, ls='-', c='0.5')
+    ax.plot(t, r_303, ls='--', c='0.5')
+
+    ax.set_xlabel('t [s]')
+    ax.set_ylabel(r'r [$\mu m$]')
+
+    ax.set_xticks([0.5E4, 1.5E4, 2.5E4, 3.5E4, 4.5E4])
+
+    ax.grid()
+
+    ax.set_ylim([0.0, 90.0])
+
+    plt.savefig('r_t_temperature.pdf')
+    #plt.show()
+
+if __name__ == '__main__':
+
+    t_start = 0.0
+    t_end = 45.0E3
+    t_delta = 1.0
+    t = np.arange(t_start, t_end, t_delta)
+
+    # plot r(t) of model vs Table 5.5
+    #plot_table(t)
+
+    # plot r(t) as temperature is varied
+    #plot_temperature(t)
